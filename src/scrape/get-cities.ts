fd29f0a20df7b13fc649345f926ac5e5c3b699da
satyawikananda <satyawikananda456@gmail.com>
2022-03-04 23:27:49 +0800

    refactor: remove some unused packages and refactor code

--- src/scrape/get-cities.ts
+++ src/scrape/get-cities.ts
@@ -6,9 +6,11 @@ import { BASE_URL } from "~/const"
 const agent = new Agent({ rejectUnauthorized: false })
 
 export const getCities = async (provinceId: string): Promise<DataCities> => {
-  const data = await axios.get(`${BASE_URL}Kabkota?kode_propinsi=${provinceId}`, { httpsAgent: agent })
+  const { data } = await axios.get(`${BASE_URL}Kabkota?kode_propinsi=${provinceId}`, {
+    httpsAgent: agent,
+  })
   return Promise.resolve({
-    cities: data.data.data.map((data: any) => ({
+    cities: data.data.map((data: any) => ({
       id: data.kode_kabkota,
       name: data.nama_kabkota,
     })),
